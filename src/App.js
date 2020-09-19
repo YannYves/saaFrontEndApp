ca8370d15e295dc5d0db95290dde7b04c6011689
yann <yanndubois4@gmail.com>
2020-09-19 18:20:56 +0200

    feat(view): add material ui pkg & react-router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React from "react";
 import "./App.css";
-import ActionComponent from "./components/ActionComponent";
-import OutputComponent from "./components/OutputComponent";
+import MenuAppBar from "./pages/homepage/MenuAppBar";
+import iconCopyRight from "./components/copyright/iconCopyRight";
+import Routes from "./Routes";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <OutputComponent />
-        <ActionComponent />
-        <div>
-          Icons made by{" "}
-          <a
-            href="https://www.flaticon.com/authors/dinosoftlabs"
-            title="DinosoftLabs"
-          >
-            DinosoftLabs
-          </a>{" "}
-          from{" "}
-          <a href="https://www.flaticon.com/" title="Flaticon">
-            www.flaticon.com
-          </a>
-        </div>
-      </header>
+      <MenuAppBar />
+      <Routes />
+      <iconCopyRight />
     </div>
   );
 }
