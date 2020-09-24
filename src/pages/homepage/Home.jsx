dad8d70b188dfd6ab00ded9ccba529442968ac78
yann <yanndubois4@gmail.com>
2020-09-24 20:56:06 +0200

    feat(view): add basic layout to homePage

--- src/pages/homepage/Home.jsx
+++ src/pages/homepage/Home.jsx
@@ -1,13 +1,67 @@
 import React from "react";
-import "./Home.css";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import MainFeaturedPost from "./MainFeaturePost";
+import FeaturedPost from "./FeaturedPost";
+import Footer from "./Footer";
 
-export default function Home() {
+const useStyles = makeStyles((theme) => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+const mainFeaturedPost = {
+  title: "Title of a longer featured blog post",
+  description:
+    "Multiple lines of text that form the , informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+  image: "https://source.unsplash.com/random",
+  imgText: "main image description",
+  linkText: "Continue reading…",
+};
+
+const featuredPosts = [
+  {
+    title: "Featured post",
+    date: "Nov 12",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    image: "https://source.unsplash.com/random",
+    imageText: "Image Text",
+  },
+  {
+    title: "Post title",
+    date: "Nov 11",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    image: "https://source.unsplash.com/random",
+    imageText: "Image Text",
+  },
+];
+
+export default function Blog() {
+  const classes = useStyles();
+
   return (
-    <div className="Home">
-      <div className="lander">
-        <h1>La vie du syndicat</h1>
-        <p>Tout sur notre communauté</p>
-      </div>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <main>
+          <MainFeaturedPost post={mainFeaturedPost} />
+          <Grid container spacing={4}>
+            {featuredPosts.map((post) => (
+              <FeaturedPost key={post.title} post={post} />
+            ))}
+          </Grid>
+          <Grid container spacing={5} className={classes.mainGrid}></Grid>
+        </main>
+      </Container>
+      <Footer
+        title="Footer"
+        description="Something here to give the footer a purpose!"
+      />
+    </React.Fragment>
   );
 }
