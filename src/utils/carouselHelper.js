4b157714142656b479d7d7b03ab7ee4cbcad89e4
yann <yanndubois4@gmail.com>
2021-01-03 18:20:01 +0100

    tmp commit

--- src/utils/carouselHelper.js
+++ src/utils/carouselHelper.js
@@ -13,8 +13,10 @@ export default function carouselHelper(data) {
       carouselObj.original = baseUrl + images.formats.large.url;
     } else if (images.formats.medium) {
       carouselObj.original = baseUrl + images.formats.medium.url;
-    } else {
+    } else if (images.formats.small) {
       carouselObj.original = baseUrl + images.formats.small.url;
+    } else {
+      carouselObj.original = baseUrl + images.url;
     }
     carouselData.push(carouselObj);
   });
